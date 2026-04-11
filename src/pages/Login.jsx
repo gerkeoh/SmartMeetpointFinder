4de2744533aadd421cc3e374e3862383a48267c5
gerkeoh <gkeohane03@gmail.com>
2026-04-11 15:57:02 +0100

    Changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { apiUrl } from "../api";
 
 const Login = () => {
   const navigate = useNavigate();
